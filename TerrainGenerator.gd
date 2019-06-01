21c15f422cd810f54348c7aa973ed3cb4ccafe47
jumbofile <gplachno@ycp.edu>
2019-05-31 20:10:00 -0400

    Updated TerrainGenerator.gd

--- TerrainGenerator.gd
+++ TerrainGenerator.gd
@@ -7,8 +7,13 @@ extends Spatial
 #Debugging bool
 var IS_DEBUGGING = true
 
+#Global vars
+var terrainWidth
+var terrainHeight
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#MAKE A CALL TO A DEFINITION CLASS THAT DEFINES WIDTH AND HEIGHT OF THE TERRAIN
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
