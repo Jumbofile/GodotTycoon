72039d97135f12e3725b025c17ca51bdaf20c63b
jumbofile <gplachno@ycp.edu>
2019-05-31 20:05:36 -0400

    Started terrain generatiopn

--- TerrainGenerator.gd
+++ TerrainGenerator.gd
@@ -1,8 +1,11 @@
 extends Spatial
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+#Class Definition:
+#This class handles terrain generation on scene startup
+#By: Gregory Plachno, 5/31/19
+
+#Debugging bool
+var IS_DEBUGGING = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,3 +14,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+#This function is just a bunch of print calls to make sure everything works
+func debug():
+	print("Nothing here")
+	
